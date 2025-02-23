9383dc66ce4569e7c444ef2797b5c81a0925c0e8
canpok1 <6isstrong@gmail.com>
2025-02-23 07:02:13 +0000

    サーバーの関数をポインタレシーバに変更

--- internal/api/impl.go
+++ internal/api/impl.go
@@ -14,25 +14,25 @@ type Server struct {
 }
 
 func NewServer(client *db.Queries) ServerInterface {
-	return Server{
+	return &Server{
 		client: client,
 	}
 }
 
 // GetApiV1CostsMonthly implements ServerInterface.
-func (s Server) GetApiV1CostsMonthly(w http.ResponseWriter, r *http.Request, params GetApiV1CostsMonthlyParams) {
+func (s *Server) GetApiV1CostsMonthly(w http.ResponseWriter, r *http.Request, params GetApiV1CostsMonthlyParams) {
 	// TODO 月次コスト取得処理を実装
 	log.Println("called GetApiV1CostsMonthly()")
 	panic("unimplemented")
 }
 
 // GetApiV1CostsTypes implements ServerInterface.
-func (s Server) GetApiV1CostsTypes(w http.ResponseWriter, r *http.Request) {
+func (s *Server) GetApiV1CostsTypes(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 
 	log.Println("called GetApiV1CostsTypes()")
 
-	resp, err := getApiV1CostsTypes(ctx, s.client)
+	resp, err := s.getApiV1CostsTypes(ctx)
 	if err != nil {
 		log.Printf("%v", err)
 		return
@@ -44,7 +44,7 @@ func (s Server) GetApiV1CostsTypes(w http.ResponseWriter, r *http.Request) {
 }
 
 // PostApiV1CostsMonthly implements ServerInterface.
-func (s Server) PostApiV1CostsMonthly(w http.ResponseWriter, r *http.Request) {
+func (s *Server) PostApiV1CostsMonthly(w http.ResponseWriter, r *http.Request) {
 	// TODO 月次コスト登録処理を実装
 	log.Println("called PostApiV1CostsMonthly()")
 	panic("unimplemented")
